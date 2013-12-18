95bbcb2633cc2f8d3d01cba51cfc42996949206a
Stuart <stuartathompson@gmail.com>
2013-12-18 15:20:26 -0500

    Reduce index to one loop option
    Removed second loop option because it wasn’t necessary — can run with
    tags or blank tags

--- themes/globenow-tiles/index.php
+++ themes/globenow-tiles/index.php
@@ -9,32 +9,39 @@
 
 			<?php 
 				$ajaxTagsCookie = $_COOKIE['globe-ajaxtags_cookie'];
-				if($ajaxTagsCookie){
+				$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+				//if($ajaxTagsCookie){
 					$args = array(
 						'posts_per_page'=>5,
-						'tag'=>$ajaxTagsCookie 		
+						'tag'=>$ajaxTagsCookie,
+						'paged'=>$paged,
+						'posts_per_page'=>10
  					);
  	
- 					$queryposts = new WP_Query( $args );
+ 					query_posts( $args );
 
-					if ($queryposts->have_posts()): while ($queryposts->have_posts()) : $queryposts->the_post();
+					if (have_posts()): while (have_posts()) : the_post();
 
 					include(locate_template('loop.php'));
 			
 					endwhile;
-		
+
+					get_template_part('pagination');
+
 					else:
 					
 					?>
 					<!-- article -->
-					<article>
+					<article class="noposts">
 						<h2><?php _e( 'No posts were found.', 'html5blank' ); ?></h2>
 					</article>	
 					<?php 
 					endif;
 					
-				} else {
+			/*	} else {
 				
+					echo 'else here';
+					
 					if (have_posts()): while (have_posts()) : the_post();
 					
 					include(locate_template('loop.php'));
@@ -51,17 +58,17 @@
 					<?php 
 					endif; 
 				
-				}
+					get_template_part('pagination');
+
+				}*/
 			?>
 	
 		</div>		
 		
-		<?php get_template_part('pagination'); ?>
 	
 	</section>
 	<!-- /section -->
 	
-
 	<?php get_sidebar(); ?>
 	<?php wp_reset_query(); ?>
 	
