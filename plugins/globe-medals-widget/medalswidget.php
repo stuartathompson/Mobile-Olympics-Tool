a2313c4c554c363bea180837eded58ef2a139b9a
Stuart <stuartathompson@gmail.com>
2014-02-08 07:08:16 -0500

    Medal widget appearance tweaks

--- plugins/globe-medals-widget/medalswidget.php
+++ plugins/globe-medals-widget/medalswidget.php
@@ -26,13 +26,6 @@ function medals_widget_create_front_end(){
 		</div>
 		<div class="clearfloat"></div>
 	<!-- place these with the rest of your CSS & JS -->
-	<script>
-		window.REUTERS_OLYMPICS_DISCIPLINE_URL = "https://olyadmin.reuters.com/sports/:discipline";
-		window.REUTERS_OLYMPICS_EVENT_URL = "https://olyadmin.reuters.com/event/:event";
-		window.REUTERS_OLYMPICS_ATHLETE_URL = "https://olyadmin.reuters.com/athlete/:athlete";
-		window.REUTERS_OLYMPICS_COUNTRY_URL = "https://olyadmin.reuters.com/team/:country";
-		window.REUTERS_OLYMPICS_HOCKEY_EVENT_URL = "";
-	</script>
 	<script src="http://oly2014-prod.s3.amazonaws.com/compiled/js/reutersOlympics.min.js"></script>
 	<script src="http://oly2014-prod.s3.amazonaws.com/dict/en-gb_globeandmail.com.js"></script>
 	<link href="http://oly2014-prod.s3.amazonaws.com/compiled/css/app.min.css" rel="stylesheet"/>
@@ -56,19 +49,12 @@ function medals_widget_create_front_end(){
 function medals_widget_create_front_end_full(){
 ?>
 	<!-- place these with the rest of your CSS & JS -->
-<!--
-	<script>
-		window.REUTERS_OLYMPICS_DISCIPLINE_URL = "https://olyadmin.reuters.com/sports/:discipline";
-		window.REUTERS_OLYMPICS_EVENT_URL = "https://olyadmin.reuters.com/event/:event";
-		window.REUTERS_OLYMPICS_ATHLETE_URL = "https://olyadmin.reuters.com/athlete/:athlete";
-		window.REUTERS_OLYMPICS_COUNTRY_URL = "https://olyadmin.reuters.com/team/:country";
-		window.REUTERS_OLYMPICS_HOCKEY_EVENT_URL = "";
-	</script>
+	
 	<script src="http://oly2014-prod.s3.amazonaws.com/compiled/js/reutersOlympics.min.js"></script>
 	<script src="http://oly2014-prod.s3.amazonaws.com/dict/en-gb_globeandmail.com.js"></script>
 	<link href="http://oly2014-prod.s3.amazonaws.com/compiled/css/app.min.css" rel="stylesheet"/>
 
-<!-- place this in your page where you want the module to appear
+<!-- place this in your page where you want the module to appear -->
 	<div id="tr_results_connection_076a560ba4f3"></div>
 	<script type="text/javascript">
 	$(document).ready(function(){
@@ -79,29 +65,7 @@ function medals_widget_create_front_end_full(){
 		});
 	});
 	</script>
--->
-	
-		<script>
-		window.REUTERS_OLYMPICS_LANGUAGE = "en-gb";
-		window.REUTERS_OLYMPICS_DISCIPLINE_URL = "http://globeandmail.com/sports/:discipline";
-		window.REUTERS_OLYMPICS_EVENT_URL = "http://globeandmail.com/event/:event";
-		window.REUTERS_OLYMPICS_COUNTRY_URL = "http://globeandmail.com/team/:country";
-		window.REUTERS_OLYMPICS_HOCKEY_EVENT_URL = "http://globeandmail.com/unit/:unit";
-	</script>
-	<script src="https://oly2014-prod.s3.amazonaws.com/compiled/js/reutersOlympics.min.js"></script>
-	<script src="https://oly2014-prod.s3.amazonaws.com/dict/en-gb_globeandmail.com.js"></script>
-	<link href="https://oly2014-prod.s3.amazonaws.com/compiled/css/app.min.css" rel="stylesheet"/>
 
-<!-- place this in your page where you want the module to appear -->
-	<div id="tr_results_connection_c9c8ac5d8b21"></div>
-	<script type="text/javascript">
-		var tr_results_connection_c9c8ac5d8b21 = new Reuters.Olympics.View.ResultsConnection({
-			"highlight": "CAN",
-			"el": "#tr_results_connection_c9c8ac5d8b21",
-			"sort": "total"
-		});
-	</script>
-	
 <?php 
 }
 
