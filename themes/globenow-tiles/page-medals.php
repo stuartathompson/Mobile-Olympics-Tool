2a5df8a8ca3fca381b42629d69df3b81ee98635c
Stuart <stuartathompson@gmail.com>
2014-02-09 12:24:17 -0500

    Ad unit on medal page

--- themes/globenow-tiles/page-medals.php
+++ themes/globenow-tiles/page-medals.php
@@ -7,7 +7,7 @@
 	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 	
 	<article class="globeabout-lede">
-<!-- 		<h1><img src="<?php bloginfo('template_url'); ?>/img/sochilogo.png" /></h1> -->
+ 		<h1 style="font-size:30px">Sochi medal count</h1>
 		<p>Keep track of Canada's place in the medal rankings throughout the Games.</p>
 		<p>This table will be updated live every time a medal is awarded.</p>
 	</article>
@@ -18,6 +18,30 @@
 			<?php if(function_exists('medals_widget_create_front_end_full')) medals_widget_create_front_end_full(); ?>
 		
 	</article>
+	
+	<?php
+				$isMobile = false;
+			if(function_exists('is_mobile')) $isMobile = is_mobile();
+			if($isMobile){
+			?>
+<div class="ad-unit-square">
+<div id="ad-unit-flex1AC"></div>
+<script type="text/javascript">
+/* mobi web */
+flex1AC=fnTktWP('a'+'1',300,50,1,'ajax',nc);
+document.getElementById('ad-unit-flex1AC').innerHTML = flex1AC;
+</script>	
+</div>
+			<?php } else { ?>
+	<div class="ad-unit-square">
+
+<div id="ad-unit-flex1AC"></div>
+<script type="text/javascript">
+flex1AC=fnTktWP('a'+'1',300,250,1,'ajax',nc);
+document.getElementById('ad-unit-flex1AC').innerHTML = flex1AC;
+</script>
+</div>
+<?php } ?>
 		<!-- /article -->
 		
 	<?php endwhile; ?>
